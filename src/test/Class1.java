d5fed399589a3219587199f3095f4a6e70e60539
Sayli Vyavhare <vyavhare.sayli@gmail.com>
2021-08-31 22:00:37 +0530

    Branch B Changes

--- src/test/Class1.java
+++ src/test/Class1.java
@@ -4,7 +4,8 @@ public class Class1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hi");
+		System.out.println("Hi456");
+		System.out.println("From Branch B");
 
 	}
 
